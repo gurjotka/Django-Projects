d4d7356d18ac32ba33a1a8f6d616bc6a9476a463
Gurjot Kaur <86513351+gurjotka@users.noreply.github.com>
2024-09-28 18:18:32 +0530

    Delete Charity_website directory

--- Charity_website/charity/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Charity
-# Register your models here.
-
-
-class CharityAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'quantity')
-
-admin.site.register(Charity, CharityAdmin)
